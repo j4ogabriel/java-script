7433c26bfedb27b197d447bad9a5d87533088906
j4ogabriel <jgcduailibe15@gmail.com>
2024-05-10 11:56:06 -0300

    utiddtuitdui
    jkgvhsuytfdtguj\gsdhzkmxcvgds8auxijdwfa8jn merda porra coco lixo

---
+++ Tapp/App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import {View,Text} from 'react-native';
+import { SingnIn } from './scr/screen/index';
+
+export default function app() {
+  return (
+    <SingnIn />
+  );
+}
