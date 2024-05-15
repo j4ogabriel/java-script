76588fbc6a105074030d13c651e6e3582e08e12d
j4ogabriel <jgcduailibe15@gmail.com>
2024-05-15 11:24:51 -0300

    feffff
    f

--- Tapp/App.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import {View,Text} from 'react-native';
-import { SingnIn } from './scr/screen/index';
-
-export default function app() {
-  return (
-    <SingnIn />
-  );
-}
